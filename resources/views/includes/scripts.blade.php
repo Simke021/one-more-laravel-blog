2351fbe8f6c930c37cdb016dda1e6ae2765b612a
Aleksandar Ljubišić <simke20@yahoo.com>
2017-09-04 18:21:54 +0200

    Addthis for shering, buttons

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -4,7 +4,8 @@
 <script src="{{ asset('app/js/theme-plugins.js') }}" ></script>
 <script src="{{ asset('app/js/main.js') }}" ></script>
 <script src="{{ asset('app/js/form-actions.js') }}" ></script>
-
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59ad7949c19998d6"></script>
 <script src="{{ asset('app/js/velocity.min.js') }}" ></script>
 <script src="{{ asset('app/js/ScrollMagic.min.js') }}" ></script>
 <script src="{{ asset('app/js/animation.velocity.min.js') }}"></script>
